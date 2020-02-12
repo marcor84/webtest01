d0809811adc89fddd3955cf3faebb764879a7c96
marcor84 <60627613+marcor84@users.noreply.github.com>
2020-02-12 21:24:21 +0100

    Update sendbymail.php

--- sendbymail.php
+++ sendbymail.php
@@ -1,36 +1,27 @@
 <?php
-if(isset($_POST['email'])) {
 
-// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "kllr46@gmail.com";
-$email_subject = "Contacto desde el sitio web";
+// Declaración de variables del formulario
+$nombre = $_POST['Nombre'];
+$email = $_POST['Email'];
+$telefono = $_POST['Telefono'];
+$mensaje = $_POST['Mensaje'];
 
-// Aquí se deberían validar los datos ingresados por el usuario
-if(!isset($_POST['first_name']) ||
-!isset($_POST['last_name']) ||
-!isset($_POST['email']) ||
-!isset($_POST['telephone']) ||
-!isset($_POST['comments'])) {
+// Datos del email
+$para = kllr46@gmail.com;
+$titulo = 'S&S Solicitud de presupuesto';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n Telefono: $telefono\n E-Mail: $email\n Mensaje:\n $mensaje";
 
-echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-die();
+if ($_POST['submit']) {
+
+    if (mail($para, $titulo, $msjCorreo, $header)) {
+
+    echo "<script language='javascript'>
+    alert('Mensaje enviado, muchas gracias por contactar con nosotros.');
+    </script>";
+    } else {
+        echo 'Falló el envio';
+    }
 }
 
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['first_name'] . "\n";
-$email_message .= "Apellido: " . $_POST['last_name'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
-$email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
-
-
-// Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-echo "¡El formulario se ha enviado con éxito!";
-}
 ?>
